ff13c571482be1fa8525d4d90e441764c16c1c1a
chenweihan <1034558980@qq.com>
2023-03-11 13:50:56 +0800

    update

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# faker
+# autoware_note
 
 
 [x] waypoint_saver
 [x] waypoint_extractor 
+[x] waypoint_loader
+[x] waypoint_replanner
+[x] waypoint_marker_publisher
--- waypoint_maker/nodes/waypoint_marker_publisher/waypoint_marker_publisher.cpp
+++ waypoint_maker/nodes/waypoint_marker_publisher/waypoint_marker_publisher.cpp
@@ -1,19 +1,6 @@
-/*
- * Copyright 2015-2019 Autoware Foundation. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
+/**
+ * 可视化其他节点发布在"final_waypoints"等话题上的轨迹。
+*/
 #include <ros/ros.h>
 #include <ros/console.h>
 #include <visualization_msgs/MarkerArray.h>
